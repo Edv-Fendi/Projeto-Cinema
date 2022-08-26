764ee6bf241d1237dbe24ceed220a5b8ddbac873
Edvaldo de Fendi <edvaldo.defendi@gmail.com>
2022-08-26 17:57:11 -0300

    Menu's

--- main.c
+++ main.c
@@ -12,42 +12,71 @@ struct Poltrona {
     int pago;
 
 };
+
 void clear (struct Poltrona poltrona[][5], int tam);
+void lePoltrona (struct Poltrona poltrona [][5], int tam);
+int  Opcaomenu ();
+void menu ();
 
 int main()
 {
 
     struct Poltrona poltrona [5][5];
-    int numMenu = 0;
+
 
     clear (poltrona, TAM);
 
 
-    printf ("-------- Cinemux -------\n");
-    printf ("------------------------\n");
-    printf ("1------- Reservar ------\n");
-    printf ("2------- Comprar -------\n");
-    printf ("3------- Cancelar ------\n");
-    printf ("4-------  Sair ---------\n");
 
+    Opcaomenu ();
+
+
+
+}
+
+void menu (){
+    printf ("-------- Cinemux  ------\n");
+    printf ("------------------------\n");
+    printf ("1------- Poltronas------\n");
+    printf ("2------- Reservar ------\n");
+    printf ("3------- Comprar  ------\n");
+    printf ("4------- Cancelar ------\n");
+    printf ("5-------  Sair    ------\n");
+    printf ("------------------------\n");
+
+}
+
+int Opcaomenu (struct Poltrona poltrona[][5]){
+    int numMenu = 0;
+    menu ();
 
     while (numMenu !=4) {
-    printf ("Digite o opcao desejada: \n");
-    scanf ("%d", &numMenu);
+
+        printf ("Digite o opcao desejada: \n");
+        scanf ("%d", &numMenu);
 
         switch (numMenu){
+
         case 1:
             printf ("Qual poltrona voce deseja reservar:\n");
+            lePoltrona (poltrona, TAM);
 
             break;
         case 2:
             printf ("Qual poltrona voce deseja comprar:\n");
+            lePoltrona (poltrona, TAM);
             break;
         case 3:
             printf ("Qual poltrona voce deseja cancelar:\n");
             break;
+
         }
+
     }
+
+    return numMenu;
+
+
 
 }
 void clear (struct Poltrona poltrona[][5], int tam){
@@ -57,4 +86,22 @@ void clear (struct Poltrona poltrona[][5], int tam){
         poltrona[i][j].pago = 0;
             }
     }
+}
+
+void lePoltrona (struct Poltrona poltrona [][5], int tam){
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    WORD saved_attributes;
+
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    saved_attributes = consoleInfo.wAttributes;
+
+    for(int i=0; i<tam; i++) {
+            for (int j=0; j < tam; j++){
+            SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+            printf("[%c][%d]   ",i + 'a', j+1);
+            }
+            printf ("\n");
+    }
+    SetConsoleTextAttribute(hConsole, saved_attributes);
 }
